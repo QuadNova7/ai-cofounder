982964a5800a105f89162c957c4793215e61b226
ChanulPathirana <chaanpathi004@gmail.com>
2026-09-12 20:25:46 +0530

    feat: monthly subscription calculator

--- services/finance/app/engine/calculator.py
+++ services/finance/app/engine/calculator.py
@@ -11,3 +11,33 @@ def calculate_projections(starting_customers: int, monthly_price: float, growth_
         })
         customers = customers * (1 + growth_rate - churn_rate)
     return projections
+
+def calculate_subscription_month(
+        monthly_price :float,
+        paying_customer:int,
+        monthly_fixed_cost:float,
+        monthly_cost_per_customer:float,
+
+        
+):
+    #caculate revenue 
+    revenue = monthly_price*paying_customer
+
+    #calculate varaible cost
+    variable_cost = monthly_cost_per_customer * paying_customer
+
+    #calculate total cost
+    total_cost = monthly_fixed_cost + variable_cost
+
+    #calculate profit 
+    profit = revenue - total_cost
+
+    return {
+        "revenue": revenue,
+        "variable_cost": variable_cost,
+        "total_cost": total_cost,
+        "modelled_profit": profit,
+    }
+
+
+
